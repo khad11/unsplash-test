ea61dd65dd5c48f9da50be4ce5d4834a993b7ed8
khadir <tolibxonovm@gmail.com>
2025-02-09 16:48:44 +0500

    implement search functionality in Home page and update Search component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { Home, About, Contact } from "./pages";
 // layout
 import MainLayout from "./layouts/MainLayout";
 
+// actions
+
+import { action as HomeAction } from "./pages/Home";
 function App() {
   const routes = createBrowserRouter([
     {
@@ -15,6 +18,7 @@ function App() {
         {
           index: true,
           element: <Home />,
+          action: HomeAction,
         },
         { path: "/about", element: <About /> },
         { path: "/contact", element: <Contact /> },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { useActionData } from "react-router-dom";
 import { Search } from "../components";
+
+// rrd
+
+// action funksiyasi
+export const action = async ({ request }) => {
+  let formData = await request.formData();
+  let search = formData.get("search");
+  return search;
+};
+
 function Home() {
+  const searchParamsFromAction = useActionData();
+
   return (
     <div>
       <div className="mt-5">
